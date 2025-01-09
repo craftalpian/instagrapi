0cc04f12c3d48648a9028fc4fe98384eacd4b78f
Pian <alfiananandaputra@gmail.com>
2025-01-09 18:47:29 +0700

    refac: remove debugging comment

--- instagrapicustom/mixins/location.py
+++ instagrapicustom/mixins/location.py
@@ -306,7 +306,6 @@ class LocationMixin:
         if max_id:
             try:
                 [m_id, page_id, nm_ids] = json.loads(base64.b64decode(max_id))
-                print("m_id, page_id, nm_ids", m_id, page_id, nm_ids)
             except Exception:
                 raise WrongCursorError()
             data["max_id"] = m_id
@@ -357,7 +356,6 @@ class LocationMixin:
             tab_key in tab_keys_v1
         ), f'You must specify one of the options for "tab_key" {tab_keys_a1}'
         medias, next_id = self.location_medias_v1_chunk(location_pk, amount, tab_key, next_max_id)
-        print("ini next_id", next_id)
         if amount:
             medias = medias[:amount]
         return medias, next_id
@@ -498,14 +496,12 @@ class LocationMixin:
             List of objects of Media
         """
         try:
-            print("Pake yang atas")
             return self.location_medias_recent_a1(location_pk, amount, sleep)
         except Exception:
             # Users do not understand the output of such information and create bug reports
             # such this - https://github.com/subzeroid/instagrapi/issues/364
             # if not isinstance(e, ClientError):
             #     self.logger.exception(e)
-            print("pake yang location_medias_recent_v1")
             return self.location_medias_recent_v1(location_pk, amount, next_max_id)
 
     def location_guides_v1(self, location_pk: int) -> List[Guide]:
